15080e6da51dffceb9f1ab5301af12ae9ff3d37b
agatakolohe <agatakolohe@gmail.com>
2020-09-05 16:05:13 -0700

    add otter img, add fadetoggle effect in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+  
+  $("p").click(function() {
+    $("#otter-showing").fadeToggle();
+    $("#otter-hidden").fadeToggle();
   });
 }); 
